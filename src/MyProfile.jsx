b045721ef0f97267440e6d6dcbe1a4eb0e23496a
punithm5 <punithlokesh42@gmail.com>
2026-01-10 23:47:08 +0530

    10/01/26

---
+++ src/MyProfile.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+/*
+class App extends Component {
+    render() {
+        return <h1>Punith</h1>
+    }
+}
+export default App
+*/
+
+const App = () => {
+    return <h3>Java Full Stack</h3>
+}
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import App from "./MyProfile"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Punith</h1>)
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
